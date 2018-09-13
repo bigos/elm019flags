9642bfa37ab4ca1809875e62dbe50cf682f3bf27
Jacek Podkanski <jacek.podkanski@conceptlifesciences.com>
2018-09-13 11:31:03 +0100

    progress with multiple polygons

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,12 @@
 module Main exposing (Flags, Model, Msg(..), init, main, update, view)
 
 import Browser
+import Frame2d exposing (Frame2d)
 import Geometry.Svg as Svg
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Point2d exposing (Point2d)
+import Polygon2d exposing (Polygon2d)
 import Svg exposing (Svg)
 import Svg.Attributes as Attributes exposing (..)
 import Triangle2d exposing (Triangle2d)
@@ -92,10 +94,57 @@ triangle =
         , Attributes.fill "orange"
         ]
         (Triangle2d.fromVertices
-            ( Point2d.fromCoordinates ( 5, 5 )
-            , Point2d.fromCoordinates ( 5, 50 )
-            , Point2d.fromCoordinates ( 100, 100 )
+            ( Point2d.fromCoordinates ( 0, 0 )
+            , Point2d.fromCoordinates ( 60, 5 )
+            , Point2d.fromCoordinates ( 5, 60 )
             )
+        )
+
+
+vertices =
+    [ Point2d.fromCoordinates ( 0, 0 )
+    , Point2d.fromCoordinates ( 50, 0 )
+    , Point2d.fromCoordinates ( 50, 50 )
+    , Point2d.fromCoordinates ( 0, 50 )
+    ]
+
+
+stamp =
+    Svg.polygon2d
+        [ Attributes.fill "yellow"
+        , Attributes.stroke "blue"
+        , Attributes.strokeWidth "2"
+        ]
+        (Polygon2d.singleLoop vertices)
+
+
+frames =
+    [ Frame2d.atPoint
+        (Point2d.fromCoordinates ( 0, 0 ))
+    , Frame2d.atPoint
+        (Point2d.fromCoordinates ( 100, 100 ))
+    , Frame2d.atPoint
+        (Point2d.fromCoordinates ( -100, 200 ))
+
+    -- , Frame2d.atPoint
+    --     (Point2d.fromCoordinates ( 175, 25 ))
+    --     |> Frame2d.rotateBy (degrees 20)
+    -- , Frame2d.atPoint
+    --     (Point2d.fromCoordinates ( 25, 150 ))
+    -- , Frame2d.atPoint
+    --     (Point2d.fromCoordinates ( 100, 100 ))
+    --     |> Frame2d.rotateBy (degrees 20)
+    -- , Frame2d.atPoint
+    --     (Point2d.fromCoordinates ( 150, 150 ))
+    --     |> Frame2d.rotateBy (degrees -30)
+    ]
+
+
+placed =
+    Svg.g []
+        (frames
+            |> List.map
+                (\frame -> Svg.placeIn frame stamp)
         )
 
 
@@ -103,11 +152,12 @@ view model =
     div []
         [ div []
             [ Svg.svg
-                [ width "120"
-                , height "120"
-                , viewBox "0 0 120 120"
+                [ width "420"
+                , height "220"
+                , viewBox "0 0 620 620"
+                , style "border: solid red 1px;"
                 ]
-                [ triangle ]
+                [ placed ]
             ]
         , button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (String.fromInt model.level) ]
