c527a1bbba5bcb40b08c3b37b7ad0238464b6292
Jacek Podkanski <ruby.object@googlemail.com>
2018-09-13 21:07:40 +0100

    shapes

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,14 @@
 module Main exposing (Flags, Msg(..), main, update, view)
 
 import Browser
+import Frame2d exposing (Frame2d)
+import Geometry.Svg as Svg
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Point2d exposing (Point2d)
+import Polygon2d exposing (Polygon2d)
+import Svg exposing (Svg)
+import Svg.Attributes as Attributes
 
 
 main =
@@ -54,9 +60,54 @@ update msg model =
             ( { model | level = model.level - 1 }, Cmd.none )
 
 
+vertices =
+    [ Point2d.fromCoordinates ( 5, 5 )
+    , Point2d.fromCoordinates ( 20, 5 )
+    , Point2d.fromCoordinates ( 5, 20 )
+    ]
+
+
+stamp =
+    Svg.polygon2d
+        [ Attributes.fill "orange"
+        , Attributes.stroke "blue"
+        , Attributes.strokeWidth "2"
+        ]
+        (Polygon2d.singleLoop vertices)
+
+
+frames =
+    [ Frame2d.atPoint
+        (Point2d.fromCoordinates ( 25, 25 ))
+    , Frame2d.atPoint
+        (Point2d.fromCoordinates ( 100, 25 ))
+    , Frame2d.atPoint
+        (Point2d.fromCoordinates ( 175, 25 ))
+        |> Frame2d.rotateBy (degrees 20)
+    , Frame2d.atPoint
+        (Point2d.fromCoordinates ( 25, 150 ))
+    , Frame2d.atPoint
+        (Point2d.fromCoordinates ( 100, 100 ))
+        |> Frame2d.rotateBy (degrees 20)
+    , Frame2d.atPoint
+        (Point2d.fromCoordinates ( 150, 150 ))
+        |> Frame2d.rotateBy (degrees -30)
+    ]
+
+
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (String.fromInt model.level) ]
         , button [ onClick Increment ] [ text "+" ]
+        , div []
+            [ Svg.svg [ Attributes.viewBox "0 0 600 600" ]
+                [ Svg.g
+                    []
+                    (frames
+                        |> List.map
+                            (\frame -> Svg.placeIn frame stamp)
+                    )
+                ]
+            ]
         ]
