1e1000b561e0e99dcd0b94f55a2baf63a98e4157
Jacek Podkanski <jacek.podkanski@conceptlifesciences.com>
2019-01-10 16:57:23 +0000

    bug fixed on the back end

--- src/Main.elm
+++ src/Main.elm
@@ -15,11 +15,3 @@ main =
         , subscriptions = subscriptions
         , view = view
         }
-
-
-
--- interesting bug TODO
--- http://localhost:3000/analytes/combined/dating_from/2016-12-21/dating_to/2017-12-22/analyte_ids/33920,%2020230,%2041843,%2038239,%2038305
--- inconsistently labels legend
--- consider cases where one of the analytes has zero data poins in the givcen
--- period, could the next one be showing in its place?
