e47993df8bd8d5393e4cd0778c6c39b597ae65bc
Jacek Podkanski <jacek.podkanski@conceptlifesciences.com>
2019-01-10 12:38:31 +0000

    a bug

--- src/Main.elm
+++ src/Main.elm
@@ -15,3 +15,9 @@ main =
         , subscriptions = subscriptions
         , view = view
         }
+
+
+
+-- interesting bug TODO
+-- http://localhost:3000/analytes/combined/dating_from/2016-12-21/dating_to/2017-12-22/analyte_ids/33920,%2020230,%2041843,%2038239,%2038305
+-- inconsistently labels legend
